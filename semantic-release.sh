942f19c97b24dcb36fd8ee7673059251bf79413d
Abhilash <abhilash@ge.com>
2024-10-25 16:37:04 +0530

    fix: new feature

--- semantic-release.sh
+++ semantic-release.sh
@@ -36,7 +36,8 @@ get_latest_commit() {
 }
 
 # Call the function to get the latest commit and assign it
-LATEST_COMMIT=$(  get_latest_commit )
+get_latest_commit $LATEST_COMMIT
+#LATEST_COMMIT=$(  get_latest_commit )
 
 # Check if the latest commit is empty before proceeding
 if [[ -z "$LATEST_COMMIT" ]]; then
